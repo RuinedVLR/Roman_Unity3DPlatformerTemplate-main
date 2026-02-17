4e522dcc6bedca682dffcbaa222abae15d08847c
RuinedVLR <rzhurakhov@gmail.com>
2026-02-17 11:03:17 -0400

    test scene created

---
+++ Assets/Scripts/GrapleHookScript.cs
@@ -0,0 +1,18 @@
+using System.Threading;
+using UnityEngine;
+
+public class GrapleHookScript : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnGrapleHold(ContextCallback context)
+    {
+        if()
+    }
+}
