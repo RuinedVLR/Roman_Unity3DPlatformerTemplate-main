d58d658e31f0041a98eb3657760640c005d147f2
RuinedVLR <rzhurakhov@gmail.com>
2026-02-20 11:06:48 -0400

    grapple script around half way done

--- Assets/Scripts/GrapleHookScript.cs
+++ Assets/Scripts/GrapleHookScript.cs
@@ -1,18 +1,52 @@
 using System.Threading;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GrapleHookScript : MonoBehaviour
 {
+    [Header("Graple Hook Settings")]
+    [SerializeField] Transform player;
+    [SerializeField] Transform grappleOrigin;
+    [SerializeField] SphereCollider grappleRange;
+    LineRenderer lr;
+
+    [Header("Target Settings")]
+    [SerializeField] GameObject target;
+    [SerializeField] LayerMask whatIsGrappleable;
     
+    Vector3 targetPosition;
+
+    bool isHoldingGrapple = false;
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    void Start()
+    {
+        targetPosition = target.transform.position;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void OnGrapleHold(ContextCallback context)
+    public void OnGrappleHold(InputAction.CallbackContext context)
     {
-        if()
+        if (context.started)
+        {
+            isHoldingGrapple = true;
+        }
+        else if (context.canceled)
+        {
+            isHoldingGrapple = false;
+            Destroy(GameObject.FindWithTag("GrapleRope"));
+        }
+
+
     }
 }
